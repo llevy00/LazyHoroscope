f0963bfb77855b1b283f4e4d6c05a1034c98d0fc
llevy00 <llevy@mail.csuchico.edu>
2019-09-23 16:13:09 -0700

    asks for name

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-//Authors:
+//Authors:Lois Levy
 #include<iostream>
 #include<string>
 
 using namespace std;
+using std::string;
+using std::cout;
+using std::cin;
 
 int main()
 {
   //declare variables
+  string first = "";
+  string last = "";
 
   //get user input
+  cout<<"What is your first name?\n";
+  cin>>first;
+  cout<<"What is your last name?\n";
+  cin>>last;
+  cout<<"Well "<<first<<" "<<last<<", here's your fortune...\n";
 
   //tell fortune
+  
 
   return 0;
 }
